b0e42ad2604892c01cd3f806165cedc549e4c604
acrylic-style <me@acrylicstyle.xyz>
2020-06-23 14:31:39 +0900

    notify when player connects server

--- src/main/java/xyz/acrylicstyle/bw/BungeeWaiter.java
+++ src/main/java/xyz/acrylicstyle/bw/BungeeWaiter.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
@@ -15,10 +16,15 @@ import net.md_5.bungee.event.EventHandler;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 public class BungeeWaiter extends Plugin implements Listener {
+    public static final String PREFIX = ChatColor.GREEN + "[" + ChatColor.AQUA + "BungeeWaiter" + ChatColor.GREEN + "] " + ChatColor.YELLOW;
     public static Logger LOGGER = Logger.getLogger("BungeeWaiter");
     public static Configuration config = null;
     public static String limbo = null;
@@ -47,7 +53,7 @@ public class BungeeWaiter extends Plugin implements Listener {
                 if (args.length != 0 && args[0].equalsIgnoreCase("--force")) {
                     if (sender instanceof ProxiedPlayer) {
                         if (((ProxiedPlayer) sender).getServer().getInfo().getName().equalsIgnoreCase(target)) {
-                            sender.sendMessage(new TextComponent(ChatColor.RED + "You don't have permission to do this."));
+                            sender.sendMessage(new TextComponent(PREFIX + "You don't have permission to do this."));
                             return;
                         }
                     }
@@ -62,15 +68,15 @@ public class BungeeWaiter extends Plugin implements Listener {
                 });
             }
         });
-        getProxy().getPluginManager().registerCommand(this, new Command("event", "bungeewaiter.notification") {
+        getProxy().getPluginManager().registerCommand(this, new Command("notification", "bungeewaiter.notification") {
             @Override
             public void execute(CommandSender sender, String[] args) {
                 if (notification) {
                     notification = false;
-                    sender.sendMessage(new TextComponent("Turned off the notification for everyone."));
+                    sender.sendMessage(new TextComponent(PREFIX + "Turned off the notification for everyone."));
                 } else {
                     notification = true;
-                    sender.sendMessage(new TextComponent("Turned on the notification for everyone."));
+                    sender.sendMessage(new TextComponent(PREFIX + "Turned on the notification for everyone."));
                 }
             }
         });
@@ -83,7 +89,9 @@ public class BungeeWaiter extends Plugin implements Listener {
                     isTargetOnline = throwable == null;
                     if (isTargetOnline) {
                         getProxy().getPlayers().forEach(player -> {
-                            if (player.getServer().getInfo().getName().equalsIgnoreCase(limbo)) player.connect(getProxy().getServerInfo(target));
+                            if (player.getServer().getInfo().getName().equalsIgnoreCase(limbo)) {
+                                player.connect(getProxy().getServerInfo(target));
+                            }
                         });
                     }
                 });
@@ -94,25 +102,32 @@ public class BungeeWaiter extends Plugin implements Listener {
 
     @EventHandler
     public void onServerConnected(ServerConnectedEvent e) {
+        Server server = e.getPlayer().getServer();
+        String name = server == null || server.getInfo() == null ? "<null>" : server.getInfo().getName();
+        String target = e.getServer().getInfo().getName();
+        getProxy().getPlayers().forEach(player -> {
+            if (player.hasPermission("bungeewaiter.logging") || player.hasPermission("bungeewaiter.notification")) {
+                player.sendMessage(new TextComponent(PREFIX + e.getPlayer().getName() + ": " + name + " -> " + target));
+            }
+        });
         TimerTask t = tasks.remove(e.getPlayer().getUniqueId());
         if (t != null) t.cancel();
-        if (e.getServer().getInfo().getName().equalsIgnoreCase(limbo)) {
-            Timer timer = new Timer();
-            TimerTask task = new TimerTask() {
-                @Override
-                public void run() {
-                    if (!e.getPlayer().isConnected()) this.cancel();
-                    if (e.getPlayer().getServer() == null || e.getPlayer().getServer().getInfo() == null) {
-                        LOGGER.warning(e.getPlayer().getName() + "'s server is null");
-                        return;
-                    }
-                    if (e.getPlayer().getServer().getInfo().getName().equalsIgnoreCase(limbo)) {
-                        if (notification) e.getPlayer().sendMessage(new TextComponent(ChatColor.YELLOW + "自動でサーバーに接続されます。そのままお待ちください。"));
-                    } else this.cancel();
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if (!e.getPlayer().isConnected()) this.cancel();
+                if (e.getPlayer().getServer() == null || e.getPlayer().getServer().getInfo() == null) {
+                    LOGGER.warning(e.getPlayer().getName() + "'s server is null");
+                    return;
                 }
-            };
-            tasks.put(e.getPlayer().getUniqueId(), task);
-            timer.schedule(task, 0, 30000);
-        }
+                if (e.getPlayer().getServer().getInfo().getName().equalsIgnoreCase(limbo)) {
+                    if (notification)
+                        e.getPlayer().sendMessage(new TextComponent(ChatColor.YELLOW + "自動でサーバーに接続されます。そのままお待ちください。"));
+                } else this.cancel();
+            }
+        };
+        tasks.put(e.getPlayer().getUniqueId(), task);
+        timer.schedule(task, 100, 30000); // give a small delay
     }
 }
