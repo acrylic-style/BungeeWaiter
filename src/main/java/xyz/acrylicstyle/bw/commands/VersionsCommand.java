6dcf5a7344726709b48f3458d9dd30f326f57c2c
acrylic-style <me@acrylicstyle.xyz>
2020-08-14 14:44:38 +0900

    yeah it must be useful

--- src/main/java/xyz/acrylicstyle/bw/commands/VersionsCommand.java
+++ src/main/java/xyz/acrylicstyle/bw/commands/VersionsCommand.java
@@ -38,7 +38,7 @@ public class VersionsCommand extends Command implements TabExecutor {
             });
             CollectionList<MCVersion> versions2 = ICollectionList.asList(versions);
             versions2.sort((a, b) -> b.getProtocolVersion() - a.getProtocolVersion());
-            versions2.forEach(version -> messages.add(ChatColor.LIGHT_PURPLE + "[" + (version.isModern() ? ChatColor.GREEN : ChatColor.YELLOW) + version.getName() + ChatColor.LIGHT_PURPLE + "]" + ChatColor.WHITE + ": " + ChatColor.GREEN + players.get(version).join(ChatColor.YELLOW + ", " + ChatColor.GREEN) + "\n"));
+            versions2.forEach(version -> messages.add(ChatColor.LIGHT_PURPLE + "[" + (version.isModern() ? ChatColor.GREEN : ChatColor.YELLOW) + version.getName() + ChatColor.LIGHT_PURPLE + "] " + ChatColor.YELLOW + "(" + ChatColor.RED + players.get(version).size() + ChatColor.YELLOW + ")" + ChatColor.WHITE + ": " + ChatColor.GREEN + players.get(version).join(ChatColor.YELLOW + ", " + ChatColor.GREEN) + "\n"));
             sender.sendMessage(messages.map((Function<String, TextComponent>) TextComponent::new).toArray(new TextComponent[0]));
         } else {
             ProxiedPlayer player = ProxyServer.getInstance().getPlayer(args[0]);
