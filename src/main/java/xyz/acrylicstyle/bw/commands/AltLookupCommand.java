209dd7dae97a23bc3b5a9de00c61d4240eff22e6
acrylic-style <me@acrylicstyle.xyz>
2020-10-22 17:25:54 +0900

    improve /altlookup, and support 1.16.4 for /versions (PV may not be accurate)

--- src/main/java/xyz/acrylicstyle/bw/commands/AltLookupCommand.java
+++ src/main/java/xyz/acrylicstyle/bw/commands/AltLookupCommand.java
@@ -12,7 +12,7 @@ import xyz.acrylicstyle.bw.BungeeWaiter;
 import xyz.acrylicstyle.sql.TableData;
 import xyz.acrylicstyle.sql.options.FindOptions;
 
-import java.net.Inet4Address;
+import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
@@ -25,33 +25,66 @@ public class AltLookupCommand extends Command implements TabExecutor {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (args.length == 0) {
-            sender.sendMessage(new TextComponent(ChatColor.RED + "/altlookup <PlayerName or IP>"));
-            return;
-        }
-        InetAddress address;
-        ProxiedPlayer player = ProxyServer.getInstance().getPlayer(args[0]);
-        if (player == null) {
+        new Thread(() -> {
+            if (args.length == 0) {
+                sender.sendMessage(new TextComponent(ChatColor.RED + "/altlookup <PlayerName, UUID, or IP>"));
+                return;
+            }
+            String address = null;
+            boolean ipv4 = true;
+            ProxiedPlayer player = ProxyServer.getInstance().getPlayer(args[0]);
             try {
-                address = InetAddress.getByName(args[0]);
-            } catch (UnknownHostException e) {
-                sender.sendMessage(new TextComponent(ChatColor.RED + "Could not resolve hostname by " + ChatColor.GOLD + args[0]));
-                return;
+                TableData td = BungeeWaiter.db.lastIpV4.findOne(new FindOptions.Builder().addWhere("uuid", UUID.fromString(args[0]).toString()).build()).complete();
+                if (td == null) {
+                    sender.sendMessage(new TextComponent(ChatColor.RED + "Could not lookup IP from UUID by " + ChatColor.GOLD + args[0]));
+                    return;
+                }
+                try {
+                    InetAddress addr = InetAddress.getByName(td.getString("ip"));
+                    if (addr instanceof Inet6Address) ipv4 = false;
+                    address = addr.getHostAddress();
+                } catch (UnknownHostException ex) {
+                    throw new RuntimeException(ex);
+                }
+            } catch (IllegalArgumentException ignored) {}
+            if (address == null) {
+                if (player == null) {
+                    try {
+                        InetAddress addr = InetAddress.getByName(args[0]);
+                        if (addr instanceof Inet6Address) ipv4 = false;
+                        address = addr.getHostAddress();
+                    } catch (UnknownHostException e) {
+                        TableData td = BungeeWaiter.db.lastIpV4.findOne(new FindOptions.Builder().addWhere("name", args[0]).build()).complete();
+                        if (td == null) {
+                            sender.sendMessage(new TextComponent(ChatColor.RED + "Could not resolve hostname / player name (case sensitive) by " + ChatColor.GOLD + args[0]));
+                            return;
+                        }
+                        try {
+                            InetAddress addr = InetAddress.getByName(td.getString("ip"));
+                            if (addr instanceof Inet6Address) ipv4 = false;
+                            address = addr.getHostAddress();
+                        } catch (UnknownHostException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                } else {
+                    if (!(player.getSocketAddress() instanceof InetSocketAddress)) {
+                        sender.sendMessage(new TextComponent(ChatColor.RED + "This player isn't connecting via IP Address."));
+                        return;
+                    }
+                    InetAddress addr = ((InetSocketAddress) player.getSocketAddress()).getAddress();
+                    if (addr instanceof Inet6Address) ipv4 = false;
+                    address = addr.getHostAddress();
+                }
             }
-        } else {
-            if (!(player.getSocketAddress() instanceof InetSocketAddress)) {
-                sender.sendMessage(new TextComponent(ChatColor.RED + "This player isn't connecting via IP Address."));
-                return;
+            if (ipv4) {
+                // ipv4
+                BungeeWaiter.db.lastIpV4.findAll(new FindOptions.Builder().addWhere("ip", address).build()).then(list -> handler(list, sender, args[0])).queue();
+            } else {
+                // ipv6
+                BungeeWaiter.db.lastIpV6.findAll(new FindOptions.Builder().addWhere("ip", address).build()).then(list -> handler(list, sender, args[0])).queue();
             }
-            address = ((InetSocketAddress) player.getSocketAddress()).getAddress();
-        }
-        if (address instanceof Inet4Address) {
-            // ipv4
-            BungeeWaiter.db.lastIpV4.findAll(new FindOptions.Builder().addWhere("ip", address.getHostAddress()).build()).then(list -> handler(list, sender, args[0])).queue();
-        } else {
-            // ipv6
-            BungeeWaiter.db.lastIpV6.findAll(new FindOptions.Builder().addWhere("ip", address.getHostAddress()).build()).then(list -> handler(list, sender, args[0])).queue();
-        }
+        }).start();
     }
 
     private static Void handler(CollectionList<TableData> list, CommandSender sender, String target) {
